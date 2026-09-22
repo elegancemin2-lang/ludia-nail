0a2e903baae6350b3e9b05d987e5bb8a9310e27d
elegancemin2-lang <elegancemin2@gmail.com>
2026-09-23 08:33:27 +0900

    Cache Salon Cloud runtime as v2.3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ludia-nail-v2.2';
-const CORE=['./','index.html','styles.css','app.js','manifest.webmanifest'];
+const CACHE='ludia-nail-v2.3';
+const CORE=['./','index.html','styles.css','salon-cloud.js','app.js','manifest.webmanifest'];
 const ASSETS=['assets/icon-192.png','assets/icon-512.png','assets/nail_1.jpg','assets/nail_2.jpg','assets/nail_3.jpg','assets/nail_4.jpg','assets/nail_5.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll([...CORE,...ASSETS])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>(k.startsWith('ludia-art-')||k.startsWith('ludia-nail-'))&&k!==CACHE).map(k=>caches.delete(k))))])));

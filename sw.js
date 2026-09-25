7921750839d16689cc030489595607721b5ea9ca
elegancemin2-lang <elegancemin2@gmail.com>
2026-09-25 22:42:48 +0900

    Rotate cache for v2.52 completion build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ludia-nail-v2.51.0';
+const CACHE='ludia-nail-v2.52.0';
 const CORE=['./','index.html','styles.css','salon-cloud.js','app.js','runtime-loader.js','core-shell.js','manifest.webmanifest'];
 const ASSETS=['assets/icon-192.png','assets/icon-512.png','assets/nail_1.jpg','assets/nail_2.jpg','assets/nail_3.jpg','assets/nail_4.jpg','assets/nail_5.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled([...CORE,...ASSETS].map(x=>c.add(x)))))});
